ea011e278a351a4e3f9c004ecb2f837df495cd86
aford504 <ahmadford504@gmail.com>
2025-05-06 18:31:40 -0500

    first-slice-of-pi

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,9 +1,11 @@
 var sensorRoutes = require("./../routes/sensors");
+var actuatorRoutes = require("./../routes/actuators");
 const express = require("express");
 var app = express();
 cors = require("cors");
 app.use(cors());
 app.use("/pi/sensors", sensorRoutes);
+app.use("/pi/actuators", actuatorRoutes);
 
 app.get("/", function (req, res) {
   res.send("Root accessed");
