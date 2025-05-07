d2ba155027d724fc1606f5b1d5f954bb8fb22592
aford504 <ahmadford504@gmail.com>
2025-05-06 19:48:13 -0500

    second-slice-of-pi

--- iot-projects/second-slice-of-pi/middleware/converter.js
+++ iot-projects/second-slice-of-pi/middleware/converter.js
@@ -1,8 +1,13 @@
-const json2html = require('node-json2html');
+const json2html = require("node-json2html");
 
-module.exports = function() {
-	return function (req, res, next) {
-		// TODO 2: Create the converter function
-		
-	};
+module.exports = function () {
+  return function (req, res, next) {
+    // TODO 2: Create the converter function
+    if (req.result) {
+      if (req.accepts("html")) {
+      }
+    } else {
+      next();
+    }
+  };
 };
--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,8 +1,11 @@
+var converter = require("./../middleware/converter");
+var bodyParser = require("body-parser");
 var sensorRoutes = require("./../routes/sensors");
 var actuatorRoutes = require("./../routes/actuators");
 const express = require("express");
 var app = express();
 cors = require("cors");
+app.use(bodyParser.json());
 app.use(cors());
 app.use("/pi/sensors", sensorRoutes);
 app.use("/pi/actuators", actuatorRoutes);
@@ -14,6 +17,8 @@ app.get("/", function (req, res) {
 app.get("/pi", function (req, res) {
   res.send("Gateway accessed");
 });
+
+app.use(converter());
 
 module.exports = app;
 // I have looked through all files
